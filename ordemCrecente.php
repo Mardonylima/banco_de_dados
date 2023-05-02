72d278dc991135478be0dcf70d252d5e05bfe407
Mardonylima <127751099+Mardonylima@users.noreply.github.com>
2023-05-02 11:33:32 -0300

    Add files via upload

---
+++ ordemCrecente.php
@@ -0,0 +1,16 @@
+<?php
+/** Escreva um codigo que receba 3 valores inteiro e os exiba em ordem crescente 'os valores tem que ser diferente' */
+$valor1 = 10;
+$valor2 = 3;
+$valor3 = 14;
+
+if ($valor1 > $valor2 && $valor1 > $valor3 && $valor2 > $valor3){
+    echo ;
+}else if (){
+    echo ;
+}else if(){
+    echo ;
+}else {
+    echo ;
+}
+?> 
